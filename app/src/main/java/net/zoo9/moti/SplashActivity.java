50342b0f189e4845425635765a311f48bab6af3a
Sheldon <unyongchoi@gmail.com>
2016-03-30 00:24:56 +0900

    Initial commit

---
+++ app/src/main/java/net/zoo9/moti/SplashActivity.java
@@ -0,0 +1,31 @@
+package net.zoo9.moti;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    private final int SPLASH_TIME_OUT_IN_SEC = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT_IN_SEC * 1000);
+    }
+}
